c51971a451b74e510c70251a977c20bf09368999
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-11-16 10:47:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function toCase(text) {
-  // write your code here
+
+	const upper= text.toUpperCase();
+	const lower = text.toLowerCase();
+	return `${lower}-${upper}`
 }
 
 // DO not change the code below
